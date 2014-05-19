88905e815b286217eb9bd0ca3c53878ec3ce2502
Ryan Bagwell <ryan@ryanbagwell.com>
2014-05-19 11:07:40 -0500

    adding moisture level to email notification

--- watch.py
+++ watch.py
@@ -6,9 +6,9 @@ garden = Garden()
 moisture = garden.read_mcp3008(channel_num=0)
 
 if moisture < garden.moisture_threshold:
-    garden.notify("Stopped watering the garden.")
+    garden.notify("Stopped watering the garden. Moisture level: %s." % moisture")
     os.system('insteonic irrigation off')
 
 elif moisture > 900:
-	garden.notify("Started watering the garden.")
+	garden.notify("Started watering the garden. Moisture level: %s." % moisture)
 	os.system('insteonic irrigation on')
