5c0837c54fabc63ba93c7add9bd0e6f6dada82bd
Ryan Bagwell <ryan@ryanbagwell.com>
2014-05-18 19:33:31 -0500

    working on watch command

--- watch.py
+++ watch.py
@@ -1,14 +1,13 @@
 from lib.garden import Garden
+import os
 
 garden = Garden()
 
+moisture = garden.read_mcp3008(channel_num=0)
 
-while True:
-	moisture = garden.read_mcp3008(channel_num=0)
-
-	if moisture < garden.moisture_threshold:
-		garden.notify("Stopped watering the garden.")
-		os.system('insteonic irrigation off')
-	else:
-		garden.notify("Started watering the garden.")
-		os.system('insteonic irrigation on')
+if moisture < garden.moisture_threshold:
+    garden.notify("Stopped watering the garden.")
+    os.system('insteonic irrigation off')
+else:
+	garden.notify("Started watering the garden.")
+	os.system('insteonic irrigation on')
