312bedebc4ab8ff75251e3e5e78821c701e80f50
BaBaBags <ryan@ryanbagwell.com>
2014-05-31 18:29:53 -0500

    adding temp conversion

--- printlevels.py
+++ printlevels.py
@@ -4,8 +4,19 @@ import time
 garden = Garden()
 
 while True:
-    moisture1= garden.sample_mcp3008(channel_num=0)
+
+    moisture = garden.sample_mcp3008(channel_num=0)
+    
     time.sleep(1)
-    moisture2= garden.sample_mcp3008(channel_num=1)
-    print "%s %s" % (moisture1, moisture2)
+    
+    volts = garden.sample_mcp3008(channel_num=1)
+
+    millivolts = volts * (3300 / 1024)
+
+    celsius = (millivolts - 500) / 10
+
+    fahrenheit = (celsius * 9.0 / 5.0) + 32.0;
+
+    print "Moisture: %s; Temp: %s;" % (moisture, fahrenheit)
+    
     time.sleep(1)
